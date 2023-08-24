6e966a1259babcb829c3825723cf15c812733213
SShrimpman <camaraoacsn@gmail.com>
2023-08-24 09:10:27 +0100

    Added logic to go from InitialPage to TaskDashboard

--- src/components/HelloWorld.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1 class="flex justify-center text-white">Task App</h1>
-</template>
---
+++ src/components/InitialPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex justify-center h-screen">
+    <div class="grid content-center space-y-20">
+      <div class="text-white text-5xl">
+        Task APP
+      </div>
+      <div class="flex justify-center">
+        <button class="bg-red-700 hover:bg-red-500 text-white rounded-lg p-3" @click="dashboard">
+          Enter ->
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    dashboard() {
+      this.$router.push('Dashboard');
+    }
+  }
+}
+</script>
