3f03d7abac8f83314c6f01a5c68f38fd5336e938
SShrimpman <camaraoacsn@gmail.com>
2023-08-24 10:36:15 +0100

    Added Code and Styles to TaskDashboard

--- src/components/TaskDashboard.vue
+++ src/components/TaskDashboard.vue
@@ -1,11 +1,42 @@
 <template>
-    <div class="flex justify-center items-center h-screen text-white">
-        Hello
-    </div>
+        <div class="mx-auto w-700 mt-36 text-white">
+            <button class="bg-green-500 p-2 rounded-lg mb-10">Create New Task</button>
+            <div class="grid grid-cols-3 text-center rounded-t-lg bg-red-500 h-24 content-center">
+                <div v-for="title in titles" class="font-lg font-bold text-xl"> {{ title.name }} </div>
+            </div>
+            <div class="bg-black rounded-b-lg">
+                <div v-for="task in getTasks" :key="task.id" class="grid grid-cols-3 w-full text-center h-24 content-center">
+                    <div>{{ task.name }}</div>
+                    <div>{{ task.deadline }}</div>
+                    <div class="space-x-2">
+                        <button class="bg-blue-700 p-1 rounded-md">Edit</button>
+                        <button class="bg-red-700 p-1 rounded-md">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
 
 <script>
+import { taskStore } from '../store/taskStore';
+import { mapState } from 'pinia'
+
     export default {
-        
+        setup() {
+        const taskStoreT = taskStore()
+        return { taskStoreT }
+    },
+        data() {
+            return {
+                titles: [
+                    { name: "Name" },
+                    { name: "Deadline" },
+                    { name: "Actions" }
+                ]
+            }
+        },
+        computed: {
+            ...mapState(taskStore, ['getTasks'])
+        }
     }
 </script>
