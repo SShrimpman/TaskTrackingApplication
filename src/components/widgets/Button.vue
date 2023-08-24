569a68e4aa5c385142af284053824f892a290ae3
SShrimpman <camaraoacsn@gmail.com>
2023-08-24 15:05:07 +0100

    Creation of Global Widget - Button

---
+++ src/components/widgets/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="btn border-0 hover:shadow-button hover:shadow-gray-500 duration-300"> {{ text }} </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            text: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
