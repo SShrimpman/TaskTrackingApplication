0db94c7d4a18e409ecc8404cf9121d66613cf13b
SShrimpman <camaraoacsn@gmail.com>
2023-08-25 13:32:17 +0100

    Applied new width style

--- src/components/popups/Create.vue
+++ src/components/popups/Create.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="fixed flex justify-center h-full w-full top-0 left-0 items-center bg-transparentBg z-10">
         <div class="flex justify-center items-center text-white">
-                <form class="w-96 bg-gray-700 shadow-md rounded-xl px-8 pt-6 pb-8 mb-4">
-                    <div class="mb-4">
-                        <label class="block text-lg font-bold mb-2">
-                            Task Name
-                        </label>
-                        <input 
-                          class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" 
-                          type="text" 
-                          placeholder="Clean my Room"
-                          v-model="addTask.name">
-                    </div>
-                    <div class="mb-6">
-                        <label class="block text-lg font-bold mb-2">
-                            Dealine
-                        </label>
-                        <input 
-                          class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" 
-                          type="date"
-                          v-model="addTask.deadline">
-                    </div>
-                    <div class="flex items-center justify-center space-x-4">
-                        <Button :text="'Create'" class="bg-green-600 hover:bg-green-500 text-white" type="button" @click="newTask(addTask)"/>
-                        <Button :text="'Cancel'" class="bg-red-600 hover:bg-red-500 text-white" type="button" @click="cancel"/>
-                    </div>
-                </form>
+            <form class="w-500 bg-gray-700 shadow-md rounded-xl px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-lg font-bold mb-2">
+                        Task Name
+                    </label>
+                    <input 
+                        class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" 
+                        type="text" 
+                        placeholder="Clean my Room"
+                        v-model="addTask.name">
+                </div>
+                <div class="mb-6">
+                    <label class="block text-lg font-bold mb-2">
+                        Dealine
+                    </label>
+                    <input 
+                        class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" 
+                        type="date"
+                        v-model="addTask.deadline">
+                </div>
+                <div class="flex items-center justify-center space-x-4">
+                    <Button :text="'Create'" class="bg-green-700 hover:bg-green-500 text-white" type="button" @click="newTask(addTask)"/>
+                    <Button :text="'Cancel'" class="bg-red-700 hover:bg-red-500 text-white" type="button" @click="cancel"/>
+                </div>
+            </form>
         </div>
     </div>
 </template>
--- src/components/popups/Delete.vue
+++ src/components/popups/Delete.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="fixed flex justify-center h-full w-full top-0 left-0 items-center bg-transparentBg z-10">
         <div class="flex justify-center items-center text-white">
-                <div class="w-96 bg-gray-700 shadow-md rounded-xl px-8 pt-6 pb-8 mb-4">
-                    <div class="flex items-center justify-center space-x-4 text-xl mb-5">
-                        Are you sure you want to delete {{ taskToDelete.name }} task?
-                    </div>
-                    <div class="flex items-center justify-center space-x-4">
-                        <Button :text="'Delete'" class="bg-red-600 hover:bg-red-500 text-white" type="button" @click="deleteTask(taskToDelete)"/>
-                        <Button :text="'Cancel'" class="bg-blue-600 hover:bg-blue-500 text-white" type="button" @click="cancel"/>
-                    </div>
+            <div class="w-500 bg-gray-700 shadow-md rounded-xl px-8 pt-6 pb-8 mb-4">
+                <div class="flex items-center justify-center space-x-4 text-xl mb-5">
+                    Are you sure you want to delete {{ taskToDelete.name }} task?
                 </div>
+                <div class="flex items-center justify-center space-x-4">
+                    <Button :text="'Delete'" class="bg-red-700 hover:bg-red-500 text-white" type="button" @click="deleteTask(taskToDelete)"/>
+                    <Button :text="'Cancel'" class="bg-blue-700 hover:bg-blue-500 text-white" type="button" @click="cancel"/>
+                </div>
+            </div>
         </div>
     </div>
 </template>
